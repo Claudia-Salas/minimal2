49fc309883f26bf9f5f4f9f3bf9496b1cc590fc2
Claudia-Salas <141759953+Claudia-Salas@users.noreply.github.com>
2023-11-22 15:08:43 -0600

    Add files via upload

---
+++ minimal/__init__.py
@@ -0,0 +1,16 @@
+def my_sum(x, y):
+    """A function that sums. """
+    return x+y
+
+
+def my_mul(x, y):
+    """A function that multiply. """
+    return x*y
+ 
+def random (n,R):
+     C=[]
+    for i in range(n):
+        C.append(randint(-R,R))
+    return set(C)
+    
+
